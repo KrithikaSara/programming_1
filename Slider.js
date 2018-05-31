24c626214861892f00062eacc53fdc694cc46d6f
Krithika <krithisar@gmail.com>
2018-05-31 23:03:49 +0530

    3

---
+++ Slider.js
@@ -0,0 +1,24 @@
+
+
+
+(function (name, global, definition) {
+    if (typeof module !== 'undefined') module.exports = definition(name, global);
+    else if (typeof define === 'function' && typeof define.amd  === 'object') define(definition);
+    else global[name] = definition(name, global);
+  })('slider', this, function (name, global){
+
+    
+    /**
+     * Initialises with default values
+     * 
+     */
+    
+     function Slider(){
+        
+        }
+
+    Slider.prototype
+
+  });
+
+
